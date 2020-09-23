6b86718127485f8128ab89dcf94d3ec241a83f02
mdgaziur001 <jL1q^3QlKK^d>
2020-09-23 17:21:59 +0600

    Remove duplicate import

--- api/user/user.js
+++ api/user/user.js
@@ -1,6 +1,5 @@
 const userInfo = require('./userInfo');
 const userProfilePicture = require('./userProfilePicture');
-const userImage = require('./userProfilePicture');
 
 module.exports.userInfo = userInfo;
 module.exports.userProfilePicture = userProfilePicture;
