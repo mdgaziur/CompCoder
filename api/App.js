cf2630ce5a36f7122ce0a329698145b574e925a2
mdgaziur001 <jL1q^3QlKK^d>
2020-09-23 13:53:51 +0600

    Create api

---
+++ api/App.js
@@ -0,0 +1,39 @@
+//Get all the packages
+const express = require('express');
+const body_parser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
+
+//Load environment variables from the .env file
+require('dotenv').config();
+
+//Import all the routes
+const auth = require('./auth/auth');
+
+//Connect to database
+mongoose.connect('mongodb://localhost/CodeBuddy', {
+    useNewUrlParser: true, useUnifiedTopology: true,
+});
+
+//Instantiate express
+const app = express();
+
+//Config the app
+app.use(fileUpload());
+app.use(cors());
+app.use(express.json());
+app.use(body_parser.urlencoded({
+    limit: '100mb',
+    extended: false
+}))
+
+app.use('/auth/login', auth.login);
+app.use('/auth/register', auth.register);
+app.use('/auth/password_reset', auth.password_reset.resetPassword);
+app.use('/auth/password_reset', auth.password_reset.isValidResetToken);
+app.use('/auth/password_reset', auth.password_reset.sendPasswordResetEmail);
+
+//The api will run at port 1233
+app.listen(1233);
+console.log('Listening on port 1233...');
---
+++ api/errorcodes.js
@@ -0,0 +1,13 @@
+const cb_errorcodes = {
+    ALREADY_LOGGED_IN: 1,
+    FAILED_LOG_IN: 2,
+    SESSION_EXPIRED: 3,
+    WRONG_PASSWORD: 4,
+    AUTH_ERROR: 5,
+    NON_EXISTING_USER: 6,
+    INCOMPLETE_FORM_REQEUST: 7,
+    NOT_UNIQUE_DETAILS: 8,
+    INVALID_TOKEN: 9
+}
+
+module.exports = cb_errorcodes;
