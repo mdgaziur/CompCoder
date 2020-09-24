b040fccb4a577e466aabec351ed7e3e7114de9b4
mdgaziur001 <jL1q^3QlKK^d>
2020-09-24 16:18:57 +0600

    Add /userSettings.js route

--- api/user/user.js
+++ api/user/user.js
@@ -1,5 +1,7 @@
 const userInfo = require('./userInfo');
 const userProfilePicture = require('./userProfilePicture');
+const userSettings = require('./userSettings');
 
 module.exports.userInfo = userInfo;
 module.exports.userProfilePicture = userProfilePicture;
+module.exports.userSettings = userSettings;
