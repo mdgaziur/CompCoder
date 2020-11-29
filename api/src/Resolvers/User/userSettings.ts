2e655d34ffc8ff2382c75368bc346d30a8ba1f35
mdgaziur001 <mdgaziurrahmannoor@gmail.com>
2020-11-29 16:56:55 +0600

    Add mutation to change miscelleneous settings and first and lastname of user account

--- api/src/Resolvers/User/userSettings.ts
+++ api/src/Resolvers/User/userSettings.ts
@@ -1,8 +1,11 @@
+import { User } from './../../models/User';
+import { DocumentType, getModelForClass } from '@typegoose/typegoose';
 import { UserInputError } from 'apollo-server';
 import { compareSync, hashSync } from 'bcrypt';
 import { Arg, Authorized, Ctx, Mutation } from 'type-graphql';
 import { Resolver } from 'type-graphql';
 import 'reflect-metadata';
+import { isUniqueField } from '../../utils/isUniqueField';
 
 @Resolver()
 export class userSettings {
@@ -16,12 +19,12 @@ export class userSettings {
     ) {
         let user = context.user;
         let isCorrectPassword = compareSync(oldPassword, user.password);
-        if(!isCorrectPassword) {
+        if (!isCorrectPassword) {
             throw new UserInputError("Wrong password!", {
                 inputArgs: ['oldPassword']
             });
         } else {
-            if(password !== confirmPassword) {
+            if (password !== confirmPassword) {
                 throw new UserInputError("Both passwords must be equal", {
                     inputArgs: ['password', 'confirmPassword']
                 });
@@ -33,5 +36,50 @@ export class userSettings {
                 return true;
             }
         }
+    }
+    @Authorized()
+    @Mutation(() => Boolean)
+    async nameSettings(
+        @Ctx() context: any,
+        @Arg('firstName', { nullable: true }) firstName?: string,
+        @Arg('lastName', { nullable: true }) lastName?: string
+    ) {
+        let userModel = getModelForClass(User);
+        let user: DocumentType<User> = context.user;
+        if(firstName) {
+            let firstNameIsUnique = await isUniqueField(firstName, 'firstName', userModel, user);
+            if(!firstNameIsUnique) {
+                throw new UserInputError("First name must be unique!", {
+                    inputArgs: ["firstName"]
+                });
+            } else  {
+                user.firstName = firstName;
+                await user.save();
+            }
+        }
+        if(lastName) {
+            user.lastName = lastName;
+            await user.save();
+        }
+        return true;
+    }
+
+    @Authorized()
+    @Mutation(() => Boolean)
+    async miscUserSettings(
+        @Arg('address', () => String) address: string,
+        @Arg('company', () => String) company: string,
+        @Ctx() context: any
+    ) {
+        let user: DocumentType<User> = context.user;
+        if(address) {
+            user.address = address;
+            await user.save();
+        }
+        if(company) {
+            user.company = company;
+            await user.save();
+        }
+        return true; //Success!
     }
 }
