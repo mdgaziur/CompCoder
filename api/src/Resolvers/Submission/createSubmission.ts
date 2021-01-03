3edc04d24e5e1a2dd17892085ede95a59b010ef6
mdgaziur001 <mdgaziurrahmannoor@gmail.com>
2021-01-03 19:00:33 +0600

    Rewrite to be compatible with the judging engine

--- api/src/Resolvers/Submission/createSubmission.ts
+++ api/src/Resolvers/Submission/createSubmission.ts
@@ -1,7 +1,19 @@
+import { join } from "path";
+import { Problem } from "./../../models/Problem";
+import { judgeEngine } from "./../../judging-engine/index";
+import { verdicts } from "./../../judging-engine/verdicts";
+import { Submission } from "./../../models/Submission";
+import { DocumentType, getModelForClass } from "@typegoose/typegoose";
 import { problemShouldExist } from "../../REST/validators/problemShouldExist";
 // import { judge } from "../../judging-engine/index";
 import { Arg, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
-import { Submission } from "../../models/Submission";
+import { tmpdir } from "os";
+import { existsSync, mkdtempSync, readFileSync } from "fs";
+import {
+  downloadTestcasesUUID,
+  downloadTestcases,
+} from "../../utils/testcaseDownloader";
+import { UserInputError } from "apollo-server";
 
 @Resolver()
 export class createSubmission {
@@ -10,27 +22,129 @@ export class createSubmission {
   async createSubmission(
     @Arg("problemId", () => String) problemId: string,
     @Arg("code", () => String) code: string,
-    @Arg("languageId", () => Number) languageId: number,
+    @Arg("languageId", () => Number) languageId: 0 | 1 | 2 | 3 | 4 | 5 | 6,
+    @Arg("testcaseType", () => String) testcaseType: "full" | "sample",
     @Ctx() context: any
   ) {
-    let inputErrors: any = [];
-    if (languageId < 1 || languageId > 10) {
-      inputErrors.append({
-        field: "languageId",
-        messsage: "Unknown langauge!",
+    if (languageId < 0 || languageId > 6) {
+      throw new UserInputError("Unknown Language!", {
+        inputArg: ["languageId"],
+      });
+    }
+    if (!["full", "sample"].includes(testcaseType)) {
+      throw new UserInputError("Unknown testcase type!", {
+        inputArg: ["testcaseType"],
       });
     }
     if (!(await problemShouldExist(problemId))) {
-      inputErrors.append({
-        field: "languageId",
-        message: "Problem does not exists!",
+      throw new UserInputError("Problem does not exists!", {
+        inputArg: ["testcaseType"],
       });
     }
-    if (inputErrors.length > 0) {
-      return inputErrors;
+    let problemModel = getModelForClass(Problem);
+    let problem: DocumentType<Problem> = await problemModel.findOne({
+      problemId: problemId,
+    });
+    let submissionModel = getModelForClass(Submission);
+    let submission: DocumentType<Submission>;
+    if (testcaseType === "sample") {
+      submission = await submissionModel.create({
+        problem: problemId,
+        author: context.user,
+        sourceFileData: code,
+        verdict: verdicts.JUDGE_Q,
+        isSampleSubmission: true,
+      });
+    } else {
+      submission = await submissionModel.create({
+        problem: problemId,
+        author: context.user,
+        sourceFileData: code,
+        verdict: verdicts.JUDGE_Q,
+        isSampleSubmission: false,
+      });
     }
-    // return await judge(problemId, code, context.user.id, languageId, "full");
-    console.log(code, context);
-    return new Submission();
+    if (!problem.testcasesMeta || !problem.sampleTestcasesMeta) {
+      throw new UserInputError(
+        "Problem does not have sample or hidden testcase!",
+        {
+          inputArg: ["problemId"],
+        }
+      );
+    } else if (
+      problem.testcasesMeta.length === 0 ||
+      problem.sampleTestcasesMeta.length === 0
+    ) {
+      throw new UserInputError(
+        "Problem does not have sample or hidden testcase!",
+        {
+          inputArg: ["problemId"],
+        }
+      );
+    } else {
+      let testcasesDir;
+      if (process.env.NODE_ENV === "production") {
+        testcasesDir = join(tmpdir(), problemId);
+        if (existsSync(testcasesDir)) {
+          let uuidFileData = readFileSync("uuid.txt").toString();
+          let uuidFileFromCloud = await downloadTestcasesUUID(problemId);
+          if (uuidFileData !== uuidFileFromCloud) {
+            let isSuccess = await downloadTestcases(
+              problemId,
+              "sample",
+              testcasesDir,
+              problem.testcasesMeta
+            );
+            if (!isSuccess) {
+              return "Unknown Server Error";
+            }
+            let isSuccessH = await downloadTestcases(
+              problemId,
+              "hidden",
+              testcasesDir,
+              problem.testcasesMeta
+            );
+            if (!isSuccessH) {
+              return "Unknown Server Error";
+            }
+          }
+        } else {
+          mkdtempSync(testcasesDir);
+          let s = await downloadTestcases(
+            problemId,
+            "sample",
+            testcasesDir,
+            problem.sampleTestcasesMeta
+          );
+          let y = await downloadTestcases(
+            problemId,
+            "hidden",
+            testcasesDir,
+            problem.testcasesMeta
+          );
+
+          if (!s || !y) {
+            return "Unknown server error!";
+          }
+        }
+      } else {
+        testcasesDir = join("files", problemId);
+      }
+      let judger = new judgeEngine(
+        problem.testcasesMeta,
+        problem.sampleTestcasesMeta,
+        testcasesDir,
+        problem.memoryLimit,
+        problem.timeLimit,
+        submission,
+        languageId,
+        code,
+        testcaseType
+      );
+      judger.start();
+
+      problem.Submissions?.push(submission._id);
+      return submission;
+    }
   }
 }
