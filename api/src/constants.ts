a24dadb79ed6c48a71af18bce8018eaec196d07e
mdgaziur001 <mdgaziurrahmannoor@gmail.com>
2021-01-10 18:05:45 +0600

    Constants: Create some constants

---
+++ api/src/constants.ts
@@ -0,0 +1,6 @@
+export default {
+  MAX_MEMORY_LIMIT: 512, // megabytes
+  MAX_TIME_LIMIT: 10 * 60 * 1000, // milliseconds
+  MIN_MEMORY_LIMIT: 6, // megabytes
+  MIN_TIME_LIMIT: 1000, // milliseconds
+};
