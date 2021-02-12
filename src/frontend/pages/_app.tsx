486f42389af83154d221787f37a0202ffb60a0b9
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-02-12 17:28:06 +0600

    Frontend: Delete the prototype stuff

--- src/frontend/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/index.scss";
-interface propsType {
-  Component: JSX.Element;
-  pageProps: any;
-}
-
-export default function App(props: propsType) {
-  return <props.Component {...props.pageProps} />;
-}
